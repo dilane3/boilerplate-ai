34ee02c01c9b6c33d7d2fbfe42308ce7be204e1c
dilane3 <komboudilane125@gmail.com>
2023-08-09 16:55:12 +0100

    Solve route access in production by using hash route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./routes";
-import store from './gx/store/index';
 import GXProvider from "@dilane3/gx";
+import store from "./gx/store/index";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import router from "./routes";
 
 function App() {
   return (
     <GXProvider store={store}>
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          {router.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </Router>
     </GXProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../components/pages/home";
 import DashboardPage from "../components/pages/dashboard";
 import AuthPage from "../components/pages/auth";
 import TeamPage from "../components/pages/team";
 import GeneratorPage from "../components/pages/generator";
 
-const router = createBrowserRouter([
+const router = [
   {
     path: "/dashboard/writings/:id",
     element: <GeneratorPage />,
@@ -26,6 +25,6 @@ const router = createBrowserRouter([
     path: "/",
     element: <HomePage />,
   },
-]);
+];
 
 export default router;
