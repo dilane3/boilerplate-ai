dae0dbb58f298f3aeca767973a853c7bafb57f38
dilane3 <komboudilane125@gmail.com>
2023-08-12 19:40:55 +0100

    Delete writing

--- src/api/writings/index.ts
+++ src/api/writings/index.ts
@@ -1,19 +1,22 @@
 import { supabaseClient } from "..";
+import Writing from "../../entities/writing/Writing";
 
 export const writingProvider = {
   create: async (payload: any) => {
     try {
-      const { data, error } = await supabaseClient.from("writings").insert({
-        type: payload.type,
-        description: payload.description,
-        user_id: payload.ownerId,
-        deleted: false,
-        config: payload.config,
-      })
-      .select("*")
-      .single();
+      const { data, error } = await supabaseClient
+        .from("writings")
+        .insert({
+          type: payload.type,
+          description: payload.description,
+          user_id: payload.ownerId,
+          deleted: false,
+          config: payload.config,
+        })
+        .select("*")
+        .single();
 
-      console.log({ data, error })
+      console.log({ data, error });
 
       if (error) {
         return {
@@ -39,7 +42,8 @@ export const writingProvider = {
       const { data, error } = await supabaseClient
         .from("writings")
         .select("*")
-        .eq("user_id", payload.uid);
+        .eq("user_id", payload.uid)
+        .eq("deleted", false);
 
       if (error) {
         return {
@@ -58,5 +62,65 @@ export const writingProvider = {
         error,
       };
     }
-  }
+  },
+
+  update: async (payload: Writing) => {
+    try {
+      const { data, error } = await supabaseClient
+        .from("writings")
+        .update({
+          description: payload.description,
+          content: payload.content,
+          config: payload.config,
+          history: payload.history,
+          image: payload.image,
+        })
+        .eq("id", payload.id)
+        .single();
+
+      if (error) {
+        return {
+          success: false,
+          error,
+        };
+      }
+
+      return {
+        success: true,
+        data,
+      };
+    } catch (error: any) {
+      return {
+        success: false,
+        error,
+      };
+    }
+  },
+
+  delete: async (payload: any) => {
+    try {
+      const { data, error } = await supabaseClient
+        .from("writings")
+        .update({ deleted: true })
+        .eq("id", payload.id)
+        .single();
+
+      if (error) {
+        return {
+          success: false,
+          error,
+        };
+      }
+
+      return {
+        success: true,
+        data,
+      };
+    } catch (error: any) {
+      return {
+        success: false,
+        error,
+      };
+    }
+  },
 };
